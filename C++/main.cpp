acc2408c203692c97dce6f8bd24a41a9f1af370d
Digital1804 <yar.oduxowi4@gmail.com>
2024-05-20 20:52:14 +0700

    done

--- C++/main.cpp
+++ C++/main.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <fstream>
 #include <vector>
+
 using namespace std;
 
 class Table{
@@ -32,9 +33,9 @@ public:
     void setTotalTime(int total_time){
         this->total_time = total_time;
     }
-    // void setTotalPrice(int total_price){
-    //     this->total_price = total_price;
-    // }
+    void setTotalPrice(int total_price){
+        this->total_price = total_price;
+    }
     void addTotalTime(int time){
         this->total_time += time;
     }
@@ -43,14 +44,15 @@ private:
     bool isFree;
 };
 
-class Custumer{
+class Customer{
 public:
-    Custumer(string name){
+    Customer(string name){
         this->name = name;
         this->table_id = 0;
         this->start_time = "00:00";
+        this->in_club = false;
     }
-    ~Custumer(){}
+    ~Customer(){}
     string getName(){
         return this->name;
     }
@@ -66,10 +68,18 @@ public:
     void setStartTime(string start_time){
         this->start_time = start_time;
     }
+    void setInClub(bool in_club){
+        this->in_club = in_club;
+    }
+    bool getInClub(){
+        return this->in_club;
+    }
 private:
     string name, start_time;
+    bool in_club;
     int table_id;
 };
+
 
 int timeToInt(string time){
     int hours = stoi(time.substr(0, 2));
@@ -78,18 +88,23 @@ int timeToInt(string time){
 }
 
 string intToTime(int time){
-    int hours = time / 60;
-    int minutes = time % 60;
-    return to_string(hours) + ":" + to_string(minutes);
+    string hours = to_string(time / 60); 
+    string minutes = to_string(time % 60);
+    if (hours.size() == 1){
+        hours = '0' + hours;
+    }
+    if (minutes.size() == 1){
+        minutes = '0' + minutes;
+    }
+    return hours + ":" + minutes;
 }
 
-void freeTable(Table table){
-    table.setIsFree(true);
+int timeOnTable(Customer Customer, string end_time){
+    return timeToInt(end_time) - timeToInt(Customer.getStartTime());
 }
-
-int findCustumerID(vector<Custumer> custumers, string name){
-    for (int i = 0; i < custumers.size(); i++){
-        if (custumers[i].getName() == name){
+int findCustomerID(vector<Customer> customers, string name){
+    for (int i = 0; i < customers.size(); i++){
+        if (customers[i].getName() == name){
             return i;
         }
     }
@@ -107,9 +122,9 @@ int countFreeTables(vector<Table> tables){
 }
 
 int countPrice(int time, int price){
-    return time * price;
+    return (time/60+1) * price;
 }
-string requests_handler(vector<Table> &tables, vector<Custumer> &custumers, string line, string begin_time, string end_time, int count_of_tables, vector<string> &queue){
+string requests_handler(vector<Table> &tables, vector<Customer> &customers, string line, string begin_time, string end_time, int count_of_tables, vector<string> &queue){
     string start_time = line.substr(0, 5);
     string res, cust_name;
     int id = (int)line[6]-48, table_id, cust_id;
@@ -117,34 +132,34 @@ string requests_handler(vector<Table> &tables, vector<Custumer> &custumers, stri
     switch (id){
         case 1:
             if (timeToInt(start_time) >= timeToInt(begin_time) && timeToInt(start_time) <= timeToInt(end_time)){
-                custumers.push_back(Custumer(line.substr(8, line.length()-8)));
+                customers.push_back(Customer(line.substr(8, line.length()-8)));
+                customers[customers.size()-1].setInClub(true);
             }
             else{
                 res += start_time + ' ' + "13 NotOpenYet\n";
             }
             break;
         case 2:
-            table_id = (int)line[line.length()-1]-48;
+            table_id = (int)line[line.length()-1]-48-1;
             cust_name = line.substr(8, line.rfind(' ')-8);
-            cust_id = findCustumerID(custumers, cust_name);
+            cust_id = findCustomerID(customers, cust_name);
             if (cust_id == -1){
                 res += start_time + ' ' + "13 ClientUnknown\n";
             }
-            else if (!tables[table_id-1].getIsFree()){
+            else if (!tables[table_id].getIsFree()){
                 res += start_time + ' ' + "13 PlaceIsBusy\n";
             }
             else {
-                custumers[cust_id].setTableId(table_id);
-                custumers[cust_id].setStartTime(start_time);
-                tables[table_id-1].setIsFree(false);
+                customers[cust_id].setTableId(table_id);
+                customers[cust_id].setStartTime(start_time);
+                tables[table_id].setIsFree(false);
             }
             break;
         case 3:
             if (countFreeTables(tables) != 0){
-                queue.push_back(line.substr(8, line.rfind(' ')-8));
                 res += start_time + ' ' + "13 ICanWaitNoLonger!\n";
             }
-            if (queue.size() < count_of_tables){
+            else if (queue.size() < count_of_tables){
                 queue.push_back(line.substr(8, line.rfind(' ')-8));
             }
             else{
@@ -153,17 +168,24 @@ string requests_handler(vector<Table> &tables, vector<Custumer> &custumers, stri
             break;
         case 4:
             cust_name = line.substr(8, line.rfind(' ')-8);
-            cust_id = findCustumerID(custumers, cust_name);
+            cust_id = findCustomerID(customers, cust_name);
             if (cust_id == -1){
                 res += start_time + ' ' + "13 ClientUnknown\n";
             }
             else{
-                
-                cust_name = custumers[0].getName();
-                custumers.erase(custumers.begin());
-                custumers[cust_id].setTableId(table_id);
-                custumers[cust_id].setStartTime(start_time);
-                tables[table_id-1].setIsFree(false);
+                table_id = customers[cust_id].getTableId();
+                tables[table_id].addTotalTime(timeOnTable(customers[cust_id], line.substr(0,5)));
+                tables[table_id].setIsFree(true);
+                customers[cust_id].setInClub(false);
+                if (queue.size() > 0){
+                    cust_name = queue[0];
+                    queue.erase(queue.begin());
+                    cust_id = findCustomerID(customers, cust_name);
+                    customers[cust_id].setTableId(table_id);
+                    customers[cust_id].setStartTime(line.substr(0,5));
+                    tables[table_id].setIsFree(false);
+                    res += start_time + " 12 " + line.substr(8, line.rfind(' ')-8) + "\n";
+                }
             }
             break;
         default:
@@ -178,11 +200,10 @@ int main(int argc, char *argv[]){
     int count_of_tables, price;
     string begin_time, end_time, line, var;  
     vector<Table> tables;
-    vector<Custumer> custumers;
+    vector<Customer> customers;
     vector<string> queue;
-    int i = 0;
-    if (myfile.is_open())
-    {
+    int line_num = 0;
+    if (myfile.is_open()){
         getline(myfile, var);
         count_of_tables = stoi(var);
         getline(myfile, begin_time, ' ');
@@ -190,14 +211,24 @@ int main(int argc, char *argv[]){
         getline(myfile, var);
         price = stoi(var);
         for (int i = 0; i < count_of_tables; i++){
-            tables.push_back(Table(i+1, price));
+            tables.push_back(Table(i, price));
         }
         myfile2 << begin_time << '\n';
         while (getline(myfile, line)){
-            myfile2 << requests_handler(tables, custumers, line, begin_time, end_time, count_of_tables, queue);
+            myfile2 << requests_handler(tables, customers, line, begin_time, end_time, count_of_tables, queue);
         }
-        myfile2 << end_time << '\n';
         myfile.close();
+        for (int i = 0; i < customers.size(); i++){
+            if (customers[i].getInClub()){
+                tables[customers[i].getTableId()].addTotalTime(timeOnTable(customers[i], end_time));
+                myfile2 << end_time + " 11" + customers[i].getName() << endl;
+            }
+        }
+        myfile2 << end_time;
+        for (int i = 0; i < tables.size(); i++){
+            tables[i].setTotalPrice(countPrice(tables[i].getTotalTime(), price));
+            myfile2 << endl << tables[i].getId()+1 << " " << tables[i].getTotalPrice() << " " << intToTime(tables[i].getTotalTime());
+        }
         myfile2.close();
     }
     else    cout << "Unable to open file";
